7835b35eaffd029d7f8086695fd3597d45f0285d
kapil tanwar <118336187+fbi786@users.noreply.github.com>
2023-11-26 11:41:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employe.c3nyaat0j8th.ap-south-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "12345678"
-customdb = "employe"
+customdb = "employee"
 custombucket = "bug12"
 customregion = "ap-south-1"
